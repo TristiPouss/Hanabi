00a5edd07fd1cecc18803928af708a3f7d9401f8
ms-lepol <maillardsalinpaul@gmail.com>
2023-11-21 15:54:34 +0100

    lobby ready - working on animations - ready to merge

--- public/js/hanabi.js
+++ public/js/hanabi.js
@@ -3,15 +3,35 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const socket = io.connect();
+    let id = null;
+
+    /**
+     *  Connexion de l'utilisateur au jeu -> Selection des lobbys.
+     */
+    function connect() {
+        console.log(sock);
+        // recupération du pseudo
+        var user = document.getElementById("pseudo").value.trim();
+        if (! user) return;
+        document.getElementById("radio2").check = true;
+        currentUser = user;
+        sock.emit("login", user);
+    }
+
+    /**
+     *  Quitter le chat et revenir à la page d'accueil.
+     */
+        function quitter() {
+            currentUser = null;
+            sock.emit("logout");
+            document.getElementById("radio1").checked = true;
+        };
+
 
     // Users List Receive
     socket.on("liste", function(l){
         if(id != null){
-            connectedUsers.innerHTML = ""; // reset
-            usersList = l;
-            usersList.forEach(user => {
-                connectedUsers.innerHTML += "<span>"+user+"</span><br>"
-            });
+
         }
     });
 
@@ -19,5 +39,14 @@ document.addEventListener("DOMContentLoaded", function() {
         // TODO
         document.getElementById("radio1").checked = false;
         document.getElementById("radio2").checked = true;
+    });
+    document.getElementById("btnCreer").addEventListener("click", function() {
+        // TODO
+        document.getElementById("radio2").checked = false;
+        document.getElementById("radio3").checked = true;
+        document.querySelectorAll(".hill0,.hill1,.hill2,.hill3,.plain,.moon").forEach(element => {
+            console.log(element.className);
+            element.setAttribute("class",element.className + " selected");
+        });
     });
 });
