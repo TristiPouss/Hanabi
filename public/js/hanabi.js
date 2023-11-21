3a67bf84b67677ed03cdab184104f4e75c8a1105
ms-lepol <maillardsalinpaul@gmail.com>
2023-11-21 12:53:54 +0100

    made background need to add animation and the lobby screen

--- public/js/hanabi.js
+++ public/js/hanabi.js
@@ -1,5 +1,9 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    document.getElementById("btnConnecter").addEventListener("click", function() {
+        // TODO
+        document.getElementById("radio1").checked = false;
+        document.getElementById("radio2").checked = true;
+    });
 });
